79d2467a03401495b007b401308d7db3e6294852
DanieL <contatodanielmrocha@gmail.com>
2023-08-01 11:30:57 -0300

    frist mod commit

--- main.py
+++ main.py
@@ -2,5 +2,30 @@ from tkinter import *
 
 root = Tk()
 
-root.mainloop()
 
+class Application():
+    def __init__(self):
+        self.root = root
+        self.tela()
+        self.frames_da_tela()
+        root.mainloop()
+    def tela(self):
+        self.root.title("Sistema SLX")
+        self.root.configure(background= "#D3D3D3")
+        self.root.geometry("700x500")
+        self.root.resizable(True, True)
+    def frames_da_tela(self):
+        self.frame_1 = Frame(self.root, bd= 4, bg= "white", 
+                             highlightbackground="#D3D3D3", highlightthickness= 2 )
+        self.frame_1.place(relx= 0.02,  rely= 0.02, relwidth= 0.97, relheight= 0.46)
+
+        self.frame_2 = Frame(self.root, bd= 4, bg= "white", 
+                             highlightbackground="#D3D3D3", highlightthickness= 2 )
+        self.frame_2.place(relx= 0.02,  rely= 0.5, relwidth= 0.97, relheight= 0.46)
+
+          
+
+
+
+
+Application()
