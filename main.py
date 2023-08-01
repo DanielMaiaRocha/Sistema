13f34eb6c1ecd121db06db1afa41f0d095b25ad8
DanieL <contatodanielmrocha@gmail.com>
2023-08-01 11:34:35 -0300

    save commit

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ root = Tk()
 class Application():
     def __init__(self):
         self.root = root
-        self.tela()
-        self.frames_da_tela()
+        self.screen()
+        self.widgets()
         root.mainloop()
-    def tela(self):
+    def screen(self):
         self.root.title("Sistema SLX")
         self.root.configure(background= "#D3D3D3")
         self.root.geometry("700x500")
         self.root.resizable(True, True)
-    def frames_da_tela(self):
+    def widgets(self):
         self.frame_1 = Frame(self.root, bd= 4, bg= "white", 
                              highlightbackground="#D3D3D3", highlightthickness= 2 )
         self.frame_1.place(relx= 0.02,  rely= 0.02, relwidth= 0.97, relheight= 0.46)
@@ -22,6 +22,11 @@ class Application():
         self.frame_2 = Frame(self.root, bd= 4, bg= "white", 
                              highlightbackground="#D3D3D3", highlightthickness= 2 )
         self.frame_2.place(relx= 0.02,  rely= 0.5, relwidth= 0.97, relheight= 0.46)
+    def buttons(self):    
+    ### Criando os botões 
+
+    
+
 
           
 
